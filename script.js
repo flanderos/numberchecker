4654b40dafe7e2cf54ac77b94962c0d839f7d9a4
flanderos <andershellerud@hotmail.no>
2023-09-09 09:27:26 +0200

    Added zettle function

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ function formatAsTable() {
 const copyButton = document.querySelector("#copy-button");
 
 copyButton.addEventListener("click", () => {
-  const copyText = missingNumbers.join("\n"); // Legg til \n for linjeskift
+  const copyText = missingNumbers.join("\n"); // add \n to paste in excel
   const textArea = document.createElement("textarea");
   textArea.value = copyText;
   document.body.appendChild(textArea);
@@ -65,3 +65,41 @@ function clearOutput() {
   inputField.value = "";
   textArea.value = "";
 }
+
+function checkZettleSeries() {
+  missingNumbers = [];
+  const zettleSeries = inputField.value.replace(/\s+/g, ",");
+  const zettleNumbers = zettleSeries.split(",").map(Number);
+
+  for (let i = 0; i < zettleNumbers.length - 1; i++) {
+    const diff = zettleNumbers[i + 1] - zettleNumbers[i];
+    const lastTwoDigits = zettleNumbers[i] % 100; // Get last two digits
+
+    if (
+      diff > 1 ||
+      (lastTwoDigits >= 31 && lastTwoDigits <= 99) ||
+      lastTwoDigits === 0
+    ) {
+      for (let j = 1; j < diff; j++) {
+        const nextNumber = zettleNumbers[i] + j;
+        const nextTwoDigits = nextNumber % 100;
+        const nextTenDigits = nextNumber % 10; // get last digit
+
+        if ((nextTwoDigits < 31 || nextTwoDigits > 99) && nextTenDigits !== 0) {
+          missingNumbers.push(nextNumber);
+        }
+      }
+    }
+  }
+
+  if (missingNumbers.length > 0) {
+    output.innerHTML = `Missing numbers in Zettle series: ${missingNumbers.join(
+      ", "
+    )}`;
+  } else {
+    output.innerHTML = "No missing numbers in Zettle series";
+  }
+}
+
+const zettleButton = document.querySelector("#zettle");
+zettleButton.addEventListener("click", checkZettleSeries);
